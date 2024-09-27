55b472b438a066b6cad1bcfaf3fa9b7faac6594a
Pratham.Shankar <prathams2108@gmail.com>
2024-09-27 09:09:34 +0530

    Second Commit

--- NATO-alphabet-start/main.py
+++ NATO-alphabet-start/main.py
@@ -14,7 +14,13 @@ print(nato_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-name=input("Enter A Name").upper()
-letters=[nato_dict[letter] for letter in name]
-print(letters)
-
+def generate_NAto():
+    name=input("Enter A Name: ").upper()
+    try:
+        letters=[nato_dict[letter] for letter in name]
+    except KeyError:
+        print("Sorry Letters only no Numbers")
+        generate_NAto()
+    else:
+        print(letters)
+generate_NAto()
